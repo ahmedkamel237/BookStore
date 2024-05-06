73e67ba68f46e09b3cfa380df53c6f5b08beb3f8
Ahmed Kamel <ahmed.kamel@baianat.com>
2024-05-06 18:48:15 +0300

    add remove collection function

--- lib/admin/domain/repository/admin_repository.dart
+++ lib/admin/domain/repository/admin_repository.dart
@@ -3,4 +3,6 @@ import 'package:stor_app/home/domain/models/categories_model.dart';
 
 abstract class AdminRepository {
   Future<void> addCollection(CategoriesModel model);
+  Future<void> removeCollection(String id);
+  // Future<CategoriesModel> updateCollection(CategoriesModel model);
 }
